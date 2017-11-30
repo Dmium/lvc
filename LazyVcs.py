bd610ea44e49922495a88d6cd2c1c961ce353e4f
dmium <dominiczy.hall@gmail.com>
2017-11-30 11:36:39 +0000

    Moved test.txt to test folder and started groundwork for diffs

--- LazyVcs.py
+++ LazyVcs.py
@@ -5,15 +5,28 @@ from watchdog.observers import Observer
 from watchdog.events import LoggingEventHandler
 from watchdog.events import FileSystemEventHandler
 
-class handler(FileSystemEventHandler):
+
+class Handler(FileSystemEventHandler):
     def on_modified(self, event):
         print(event.src_path)
+
+    def on_created(self, event):
+        pass
+
+    def diff(self, path):
+        pass
+class LineChange():
+    def __init__(self, add, line):
+        self.add = add
+        self.line = line
+
+class Commit():
+    pass
+
+
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO,
-                        format='%(asctime)s - %(message)s',
-                        datefmt='%Y-%m-%d %H:%M:%S')
     path = sys.argv[1] if len(sys.argv) > 1 else '.'
-    event_handler = handler()
+    event_handler = Handler()
     observer = Observer()
     observer.schedule(event_handler, path, recursive=True)
     observer.start()
