95e9fee1142258b82d767097340d45a8f8dbffbd
Cyrile Verdeyen <cyrile.verdeyen>
2017-12-02 14:47:22 +0000

    handler looks through file for changed file and if it is there, changes it

--- Handler.py
+++ Handler.py
@@ -2,7 +2,10 @@
 from watchdog.events import FileSystemEventHandler
 class Handler(FileSystemEventHandler):
     def on_modified(self, event):
-        if (evnt.src_path in trackedFiles)
+        with open(".file_list","r") as f:
+            contents = f.read()
+            pathlist = json.loads(contents)
+        if (event.src_path in pathlist):
             changedFile = TrackedFile.load_file(event.src_path)
             changedFile.update()
 
