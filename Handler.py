4202bb35770490d04021e200c359eadaf1d212ff
dmium <dominiczy.hall@gmail.com>
2017-12-02 15:49:09 +0000

    Fiddling to get Handler to work in progress

--- Handler.py
+++ Handler.py
@@ -2,15 +2,16 @@ from FileManager import FileManager
 from watchdog.events import FileSystemEventHandler
 from TrackedFile import TrackedFile
 class Handler(FileSystemEventHandler):
-    def __init__(self):
-        super.__init__(self)
+    def load_handler(self):
         self.file_manager = FileManager()
 
     def on_modified(self, event):
-        if (file_manager.check(event.src_path)):
+        print("Modded")
+        if (self.file_manager.check(event.src_path)):
             changedFile = TrackedFile.load_file(event.src_path)
             changedFile.update()
 
     def on_created(self, event):
-        if (!(file_manager.check(event.src_path)):
-            file_manage.track(event.src_path)
+        print("Created:")
+        if not self.file_manager.check(event.src_path):
+            self.file_manager.track(event.src_path)
