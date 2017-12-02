55c852be8046b396ebe267585e7d7f933c2f0bc7
Henry Gilbert <henrygilbert15@live.co.uk>
2017-12-02 12:23:16 +0000

    Refactor code for making a dot filepath into its own method get_dot_file_path.

--- TrackedFile.py
+++ TrackedFile.py
@@ -15,25 +15,37 @@ class TrackedFile():
     def read_new_into_current():
         current_content = read_new()
 
+
+
+    # saves a TrackedFile class to a binary file, and return the dot_file_path
     def SaveFile(self):
-        file_name = ""
-        last_fslash_index = self.file_name.rfind("/")
-        last_bslash_index = self.file_name.rfind("\\")
-        if last_fslash_index != -1:
-            file_name = self.file_name[:last_fslash_index] + "." +\
-             self.file_name[last_fslash_index:]
-        elif last_bslash_index != -1:
-            file_name = self.file_name[:last_bslash_index] + "." +\
-             self.file_name[last_bslash_index:]
-        else:
-            file_name = "." + self.file_name
-
-        with open(file_name, 'wb') as f:
+        file_path = get_dot_file_path(self.file_name)
+        print(file_path)
+        with open(file_path, 'wb') as f:
             dumped = pickle.dump(self, f)
-            return file_name
+            return file_path
 
-    def LoadFile(file_name):
-        with open(file_name, 'rb') as f:
+    # takes the path of the file whose TrackedFile class should be loaded
+    def LoadFile(file_path):
+        dot_file_path = get_dot_file_path(file_path)
+        with open(dot_file_path, 'rb') as f:
             loaded_file = pickle.load(f)
             return loaded_file
         return None
+
+
+# converts a file path to its corresponding dot file path, eg C:\example\foo.txt to C:\example\.foo.txt
+def get_dot_file_path(file_path):
+    # I am so, so sorry
+    file_name = file_path
+    last_fslash_index = file_path.rfind("/")
+    last_bslash_index = file_path.rfind("\\")
+    if last_fslash_index != -1:
+        file_name = file_path[:last_fslash_index+1] + "." +\
+         file_path[last_fslash_index+1:]
+    elif last_bslash_index != -1:
+        file_name = file_path[:last_bslash_index+1] + "." +\
+         file_path[last_bslash_index+1:]
+    else:
+        file_name = "." + file_path
+    return file_name
