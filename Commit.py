a48944daa24f220df46a6f68ef3f3709dc03e41c
dmium <dominiczy.hall@gmail.com>
2017-12-02 13:44:35 +0000

    Basic Diffing working and Commit updated to reflect this

--- Commit.py
+++ Commit.py
@@ -1,6 +1,8 @@
 class Commit():
-    def __init__(self, id, datetime, line_delta):
+    def __init__(self, id, datetime, last_commit, current_file):
         self.id = id
-        self.datetime - datetime
-        self.line_delta = []
-        self.dir_changes = []
+        self.datetime = datetime
+        self.line_delta = Diffing.get_delta(Diffing.get_version(last_commit.line_delta), current_file)
+
+    def get_version(self):
+        return Diffing.get_version(self.line_delta)
--- Diffing.py
+++ Diffing.py
@@ -1,9 +1,11 @@
-import difflib
+from difflib import Differ
+from difflib import restore
 class Diffing():
-    def getDelta(current_file, new_file):
+    # Note: Delta is reversed so we work backwards
+    def get_delta(current_file, new_file):
         current_file = current_file.splitlines(keepends=True)
         new_file = new_file.splitlines(keepends=True)
-        return list(Differ().compare(current_file, new_file)
+        return list(Differ().compare(current_file, new_file))
 
-    def reverseDelta(current_file, delta):
-        pass
+    def get_version(delta):
+        return ''.join(restore(delta, 2))
