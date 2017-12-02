7dd5aecd39bc0bf4ca3c9ca4c8085260e526c390
dmium <dominiczy.hall@gmail.com>
2017-12-02 14:11:51 +0000

    Fixed Commit constructor and refactored property names

--- Commit.py
+++ Commit.py
@@ -1,9 +1,12 @@
 from Diffing import Diffing
 class Commit():
-    def __init__(self, id, datetime, last_commit, current_file):
+    def __init__(self, id, datetime, last_commit, new_file):
         self.id = id
         self.datetime = datetime
-        self.line_delta = Diffing.get_delta(Diffing.get_version(last_commit.line_delta), current_file)
+        if last_commit is None:
+            self.line_delta = Diffing.get_delta('', current_file)
+        else:
+            self.line_delta = Diffing.get_delta(Diffing.get_version(last_commit.line_delta), current_file)
 
     def get_version(self):
         return Diffing.get_version(self.line_delta)
