1484aa6738c6a08d8ffa5e53ba106f17b0fda2bb
dmium <dominiczy.hall@gmail.com>
2017-12-02 15:28:08 +0000

    Begun testing Handler

--- Handler.py
+++ Handler.py
@@ -1,18 +1,13 @@
 from FileManager import FileManager
 from watchdog.events import FileSystemEventHandler
+from TrackedFile import TrackedFile
 class Handler(FileSystemEventHandler):
     def __init__(self):
         super.__init__(self)
         self.file_manager = FileManager()
+
     def on_modified(self, event):
-        with open(".file_list","r") as f:
-            contents = f.read()
-            pathlist = json.loads(contents)
-        if (event.src_path in pathlist):
-            changedFile = TrackedFile.load_file(event.src_path)
-            changedFile.update()
-
-
+        cFile = TrackedFile(event.src_path)
 
     def on_created(self, event):
         pass
