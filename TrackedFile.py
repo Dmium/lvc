fe5464b2d0bf88daa24720603cb2a8c92bb597db
Henry Gilbert <henrygilbert15@live.co.uk>
2017-12-02 11:24:57 +0000

    Add read_new method to just read and return content of file.

--- TrackedFile.py
+++ TrackedFile.py
@@ -3,9 +3,12 @@ class TrackedFile():
         self.file_name = file_name
         self.commits = []
         self.current_content = ""
-        read_new_into_current()
 
-    def read_new_into_current():
+    def read_new():
         new_content = current_file
         with open(file_name, 'r') as f:
             new_content = f.read()
+        return new_content
+
+    def read_new_into_current():
+        current_content = read_new()
