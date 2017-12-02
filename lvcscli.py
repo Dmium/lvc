f9e53372adac4dd4aaf297a996a6b9fcc1de7211
dmium <dominiczy.hall@gmail.com>
2017-12-02 18:55:46 +0000

    finished cli for now

--- lvcscli.py
+++ lvcscli.py
@@ -1,9 +1,10 @@
 import sys
 import os
+import webbrowser
 if len(sys.argv) > 1:
     if sys.argv[1] == "run":
         os.system('runlvcs')
     elif sys.argv[1] == "add":
         pass
     elif sys.argv[1] == "open":
-        os.system('google-chrome-stable localhost:8080')
+        webbrowser.open('localhost:8080', new=2)
