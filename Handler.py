f0168d0049593545e54c5e123128fa18cfb5ed6b
Cyrile Verdeyen <cyrile.verdeyen>
2017-12-02 12:26:58 +0000

    I ever so slightly changed handler

--- Handler.py
+++ Handler.py
@@ -2,7 +2,7 @@
 from watchdog.events import FileSystemEventHandler
 class Handler(FileSystemEventHandler):
     def on_modified(self, event):
-        print(event.src_path)
+        diff.fileopen.(event.src_path)
 
     def on_created(self, event):
         pass
