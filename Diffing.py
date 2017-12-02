61c80599b9531a75a8821f8a94f41ddd8d9dfbe6
dmium <dominiczy.hall@gmail.com>
2017-12-02 11:42:10 +0000

    Wrote simple getDelta method in Diffing

--- Diffing.py
+++ Diffing.py
@@ -1,6 +1,9 @@
+import difflib
 class Diffing():
     def getDelta(current_file, new_file):
-        pass
+        current_file = current_file.splitlines(keepends=True)
+        new_file = new_file.splitlines(keepends=True)
+        return list(Differ().compare(current_file, new_file)
 
     def reverseDelta(current_file, delta):
         pass
