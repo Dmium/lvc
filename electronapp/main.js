d747c1979f19c0e257cf39cf6c5e8d636293611c
Zack Clark-Kington <zack@finimize.com>
2017-12-15 04:09:27 +0000

    Initial work on cross platform electron app

---
+++ electronapp/main.js
@@ -0,0 +1,31 @@
+'use strict';
+
+var app = require('electron').app;
+var BrowserWindow = require('electron').BrowserWindow;
+var fs = require('fs');
+
+var mainWindow = null;
+
+app.on('ready', function() {
+	mainWindow = new BrowserWindow({
+		height: 600,
+		width: 800,
+		title: "LVC native"
+	});
+
+	mainWindow.on('closed', () => {
+		mainWindow = null
+	});
+
+	mainWindow.loadURL('file://' + __dirname + '/app/index.html');
+
+	var contents = mainWindow.webContents
+
+	fs.readFile(__dirname + '/app/css/styles.css', 'utf8', function(err, css){
+		contents.insertCSS(css);
+	});
+
+	fs.readFile(__dirname + '/app/lvc-native.min.js', 'utf8', function(err, js){
+		contents.executeJavaScript(js);
+	});
+})
